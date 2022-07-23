4b3a492c5155ca2a67dcbc6d55dea3ef03674e71
thatviper <challanaganarasimhareddy@gmail.com>
2022-07-23 21:52:00 +0530

    react-router-v6

---
+++ react-router/src/Components/Login.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "./auth";
+
+export const Login = () => {
+  const [user, setUser] = useState("");
+  const auth = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const redirectPath = location.state?.path || "/";
+
+  const handleLogin = () => {
+    auth.login(user);
+    navigate(redirectPath, { replace: true });
+  };
+
+  return (
+    <>
+      <label>
+        Username :
+        <input
+          type="text"
+          onChange={(e) => {
+            setUser(e.target.value);
+          }}
+        />
+        <button onClick={handleLogin}>Login</button>
+      </label>
+    </>
+  );
+};
