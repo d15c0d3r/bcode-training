e9385fd88e022b75bf66de2ce4eef58d84dba58c
thatviper <challanaganarasimhareddy@gmail.com>
2022-08-17 11:04:14 +0530

    controllers added into server

--- my-app/src/apis/ProductApis.js
+++ my-app/src/apis/ProductApis.js
@@ -1,13 +1,16 @@
 import axios from "axios";
+export const herokuDomain = `https://ecommerce-server-training.herokuapp.com`;
+export const localDomain = `http://localhost:5000`;
 
 export const getAllProducts = async () => {
-  const { data } = await axios.get(`http://localhost:5000/api/products/`);
+  const { data } = await axios.get(`${localDomain}/api/products/`);
   return data;
 };
 
 export const getProduct = async ({ queryKey }) => {
   const { data } = await axios.get(
-    `http://localhost:5000/api/products/${queryKey[1]}`
+    `${localDomain}/api/products/${queryKey[1]}`
   );
+  console.log(data);
   return data;
 };
--- my-app/src/apis/ReviewsApi.js
+++ my-app/src/apis/ReviewsApi.js
@@ -1,18 +1,14 @@
 import axios from "axios";
+import { herokuDomain, localDomain } from "./ProductApis";
 
 export const getProductReviews = async ({ queryKey }) => {
   console.log("triggered", queryKey[1]);
-  const { data } = await axios.get(
-    `http://localhost:5000/api/review/${queryKey[1]}`
-  );
+  const { data } = await axios.get(`${localDomain}/api/review/${queryKey[1]}`);
   console.log("product :", data);
   return data;
 };
 
 export const addProductReview = ({ id: productId, review }) => {
-  const data = axios.post(
-    `http://localhost:5000/api/review/${productId}`,
-    review
-  );
+  const data = axios.post(`${localDomain}/api/review/${productId}`, review);
   return data;
 };
