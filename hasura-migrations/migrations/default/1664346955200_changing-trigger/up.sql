494205759118ae4c5bc69f196c476f91d8394e8f
thatviper <challanaganarasimhareddy@gmail.com>
2022-09-28 12:31:33 +0530

    cmd <npx hasura migrate apply --version {version_timestamp-number} --database-name {default} migration of that version is applied to the database mentioned>

--- hasura-migrations/migrations/default/1664346955200_changing-trigger/up.sql
+++ hasura-migrations/migrations/default/1664346955200_changing-trigger/up.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE FUNCTION public.check_user_name() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
+    
 begin
     if length(new.name) < 5 then
         raise exception 'user name should be atleast of length 5!';
